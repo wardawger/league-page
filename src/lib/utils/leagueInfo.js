a6c96d4fbdf20bbd8c15c56a65a2954f62e1a5ef
wardawger <150193457+wardawger@users.noreply.github.com>
2024-03-19 17:00:22 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -91,7 +91,7 @@ export const managers = [
       "tradingScale": 10, // 1 - 10
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
-  ]
+
   
   
   /*   !!  !!  IMPORTANT  !!  !! */
@@ -262,3 +262,4 @@ export const managers = [
       "tradingScale": 10, // 1 - 10 (optional)
       "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },             
+  ]
